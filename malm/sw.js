a803c0c037eccca43d85466edaaaf209b80df680
matanaor1987-lgtm <matanaor1987@gmail.com>
2026-06-02 10:45:45 +0300

    SW v2 - always fetch fresh, clear cache

--- malm/sw.js
+++ malm/sw.js
@@ -1,3 +1,18 @@
-self.addEventListener('install', function(e) { self.skipWaiting(); });
-self.addEventListener('activate', function(e) { return self.clients.claim(); });
-self.addEventListener('fetch', function(e) {});
+const VERSION = '2.0';
+
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.map(function(k) { return caches.delete(k); }));
+    }).then(function() { return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  // Always fetch fresh from network
+  e.respondWith(fetch(e.request));
+});
