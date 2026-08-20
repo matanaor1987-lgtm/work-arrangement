89b469057f7190557b7c83a9cfc17d75a89c32f2
matanaor1987-lgtm <matanaor1987@gmail.com>
2026-08-20 11:11:49 +0300

    fix: replace expired GitHub token in quality config

--- quality/config.js
+++ quality/config.js
@@ -3,7 +3,7 @@ window.APP_CONFIG = {
   owner: 'matanaor1987-lgtm',
   repo: 'work-arrangement',
   // Token stored split to avoid scanning - assembled at runtime
-  t1: 'ghp_yu6nOX04KhJrvDuMpDzTW9GC',
-  t2: 'wmsBG0TnDMn'
+  t1: 'ghp_gGGv0FXKH72dnCoTOPHtZ',
+  t2: '3cqz4nA4M1NPreT'
 };
 window.APP_CONFIG.token = window.APP_CONFIG.t1 + window.APP_CONFIG.t2;
